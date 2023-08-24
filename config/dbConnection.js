62338aacbb275bd029581daba8c035ad7b46952d
102AMIT <kingston.amit9830@gmail.com>
2023-08-24 13:54:35 +0530

    changes done for aws

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const connection = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "Amit9830",
+    password: "Admin123456789!",
     database: "eccomerce",
     port: 3306, // Note the corrected property name
   });
